122f063718765be159cdec106d4b2d8ee2cd41f4
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-01-31 18:50:57 -0500

    Done.

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,14 @@
 import React from "react";
+import * as asdf from "../data/user"
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src={asdf.image} alt="I made this" />
     </div>
   );
 }
+
+export default About
